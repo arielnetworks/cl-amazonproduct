c394c2d1d639181d61d9e2af58445278bd688d6f
fukamachi <e.arrows@gmail.com>
2010-12-28 20:11:32 +0900

    added declarations to utilities.

--- util.lisp
+++ util.lisp
@@ -2,9 +2,16 @@
 
 (cl-interpol:enable-interpol-syntax)
 
+(declaim (inline string-camelcase))
 (defun string-camelcase (val)
+  (declare (optimize speed)
+           (type string val))
   "Return camelcased name."
-  (remove #\- (string-capitalize val)))
+  (the string (remove #\- (string-capitalize val))))
 
+(declaim (inline string-join))
 (defun string-join (delimiter string-list)
-  (format nil #?"~{~A~^${delimiter}~}" string-list))
+  (declare (optimize speed)
+           (type character delimiter)
+           (type list string-list))
+  (the string (format nil #?"~{~A~^${delimiter}~}" string-list)))
